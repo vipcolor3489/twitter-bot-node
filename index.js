c1be69f6bfb8a74cb21119a4ca4384fc64aa3dc5
vipcolor3489 <vipcolor3489@gmail.com>
2025-05-11 12:46:53 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
 const axios = require('axios');
-const http = require('http'); // Railway用に追加
 
 // Twitter APIクライアントの初期化
 const client = new TwitterApi({
@@ -12,7 +11,7 @@ const client = new TwitterApi({
 });
 
 const listId = process.env.LIST_ID;
-const iftttWebhookUrl = process.env.IFTTT_WEBHOOK_URL;
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
 
 // 条件にしたいキーワード
 const keywords = ['【要人発言】', '市場概況】', '【経済指標速報】', '【指標】', '【指標発表予定】'];
@@ -39,9 +38,8 @@ async function fetchAndNotify() {
         if (match) {
           console.log(`マッチしたツイート: ${text}`);
 
-          await axios.post(iftttWebhookUrl, {
-            value1: text,
-            value2: `https://twitter.com/i/web/status/${tweet.id}`
+          await axios.post(discordWebhookUrl, {
+            content: `📢 新着ツイート: ${text}\n🔗 https://twitter.com/i/web/status/${tweet.id}`
           });
 
           lastTweetId = tweet.id;
@@ -55,14 +53,5 @@ async function fetchAndNotify() {
   }
 }
 
-// 10秒ごとに監視
-setInterval(fetchAndNotify, 65000);
-
-// 🚀 Railway維持のためのダミーHTTPサーバー起動
-const PORT = process.env.PORT || 3000;
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Bot is running\n');
-}).listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+// 🔄 30秒間隔で定期実行
+setInterval(fetchAndNotify, 60000);
